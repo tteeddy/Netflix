a59c6818f4f2964742f2da98179f93ca2e1d6098
tteeddy <76959597+tteeddy@users.noreply.github.com>
2021-03-10 12:16:08 -0500

    Initial commit

---
+++ build/precache-manifest.6cefaddebf0303823f26395c5e410d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4af30669148b0c530fd99460ca4d08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e385a644ce64444655c",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "2c1b512cedcfa7ee8100",
+    "url": "/static/js/2.719f3f89.chunk.js"
+  },
+  {
+    "revision": "ee4991bbc4f3dcc561e92da49df07222",
+    "url": "/static/js/2.719f3f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e385a644ce64444655c",
+    "url": "/static/js/main.4c6947cf.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const API_KEY = "f46f3a89b600d7b3695b67541766a8ba";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
